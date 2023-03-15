797fd75ac128bd68992e23987eef80caadc3f872
MeganL314 <megantherese314@gmail.com>
2023-03-15 11:26:29 -0400

    updated RunNormals.sh

--- RunNormals.sh
+++ RunNormals.sh
@@ -2,16 +2,29 @@
 # File:	RunNormals.sh
 
 
+#$ -cwd
+# File: RunNormals.sh
+
+module load gatk/4.2.0.0
+
 arr=()
 while IFS= read -r line; do
    arr+=("$line")
-done < Normals
+done < Normals.txt
 
-echo ${arr[*]}  
+echo ${arr[*]}
+path=/directory/bams/
+reference=/directory/ref.fasta
 
 for sample in ${arr[*]}
 do
-
-	echo $sample
-	echo "  "
+  	gatk Mutect2 \
+                -R $reference \
+                -I $path$sample \
+                -O single_$sample.vcf.gz
+        echo "Sample: $sample"
+        echo "$path$sample"
 done
+
+
+
